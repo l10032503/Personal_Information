ff2cb05fd77a5a0820cdff5a2f43b96243a5ebf0
JiwonCocoder <45135820+JiwonCocoder@users.noreply.github.com>
2018-12-21 05:22:32 +0900

    Update ToDoList.java

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,19 +1,22 @@
+package com.example.junit;
+
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 public class ToDoList {
 	private int input;
-	private List<ToDoListVO> todolist_ArrayList;
+	//private List<ToDoListVO> todolist_ArrayList;
+	public List<ToDoListVO> todolist_ArrayList;
 	private ToDoListVO todolist_information;
-	private Scanner scan;
+	public Scanner scan=new Scanner(System.in);
 	public int i=0;
 	
 	public ToDoList() {
 		this.todolist_ArrayList=new ArrayList<ToDoListVO>();
-		this.input=0;
-		
+		this.input=0;		
 		this.todolist_information=new ToDoListVO("", 0, 0, "");
-		this.scan=new Scanner(System.in);
 	}
 	//Getter,Setter-todolist_ArrayList
 	public List<ToDoListVO> getTodolist_ArrayList(){
@@ -30,6 +33,10 @@ public class ToDoList {
 		this.input=input;
 	}
 	//Add
+	public int update(int index, ToDoListVO todolist_information) {
+		todolist_ArrayList.set(index, todolist_information);
+		return 1;
+	}
 	public String toString() {
 		return todolist_information.getName()+todolist_information.getDate()+todolist_information.getDue()+todolist_information.getDescription();
 	}
@@ -47,11 +54,17 @@ public class ToDoList {
 			this.setInput(scan.nextInt());
 			if(input==1) {
 				//System.out.println(getInput());
-				ToDoListCreate(todolist_information,todolist_ArrayList);				
+				this.inputInfo(todolist_information);
+				String addItemName=ToDoListCreate(todolist_information,todolist_ArrayList);				
 			}
 			else if(input==2) {
-				ToDoListView(todolist_information,todolist_ArrayList);
+				String lastItemName=ToDoListView(todolist_information,todolist_ArrayList);
 			}
+			else if(input==3) {
+				ToDoListUpdate(todolist_information,todolist_ArrayList);
+			}
+			else
+				ToDoListDelete(todolist_information,todolist_ArrayList);
 		}
 		
 	}
@@ -71,31 +84,33 @@ public class ToDoList {
 
 		return todolist_information;
 	}
-	public void ToDoListCreate(ToDoListVO ti, List<ToDoListVO> todolist_ArrayList) {
+
+	public String ToDoListCreate(ToDoListVO ti, List<ToDoListVO> todolist_ArrayList) {
 		ToDoListVO todolist_information = new ToDoListVO(ti.getName(), ti.getDate(), ti.getDue(), ti.getDescription());
-		this.inputInfo(todolist_information);
 		todolist_ArrayList.add(todolist_information);
-		//todolist_ArrayList.add(todolist_information(todolist_information.getName(),todolist_information.getDate(),todolist_information.getDue(),todolist_information.getDescription()));
-		//todolist_ArrayList.add(new ToDoListVO(todolist_information.getName(), todolist_information.getDate(), todolist_information.getDue(), todolist_information.getDescription()));
-		//todolist_ArrayList.add(new ToDoListVO("1",0,0,"1"));
 		//System.out.println(todolist_information.getName()+todolist_information.getDate()+todolist_information.getDue()+todolist_information.getDescription());
 
-		//System.out.println(todolist_ArrayList.get(i).toString());
-
-		//todolist_ArrayList.add(new ToDoListVO());
+		return ti.getName();
+		
 
 	}
 
-	public void ToDoListView(ToDoListVO todolist_information,List<ToDoListVO> todolist_ArrayList){
+	public String ToDoListView(ToDoListVO todolist_information,List<ToDoListVO> todolist_ArrayList){
 		System.out.println("name\t\tdate\tdue\tdescription\t\t\t");
 		System.out.println("===================================================");
-		/*for(ToDoListVO vo:this.getTodolist_ArrayList()) {
 
-			System.out.println(vo.getName()+vo.getDate()+vo.getDue()+vo.getDescription());
-		}*/
-//		System.out.println(todolist_ArrayList.size());
-		//System.out.println(todolist_information.getName()+todolist_information.getDate()+todolist_information.getDue()+todolist_information.getDescription());
-
+		Collections.sort(todolist_ArrayList, new Comparator<ToDoListVO>() {
+			public int compare(ToDoListVO o1, ToDoListVO o2) {
+				if(o1.getDue()>o2.getDue()) {
+					return 1;
+				}
+				else if(o1.getDue()<o2.getDue()) {
+					return -1;
+				}
+				else return 0;
+				
+			}
+		});	
 		for(int i=0; i<todolist_ArrayList.size();i++) {
 			//System.out.println(todolist_information.getName()+todolist_information.getDate()+todolist_information.getDue()+todolist_information.getDescription());
 			System.out.println(todolist_ArrayList.get(i).getName() + "\t" 
@@ -104,8 +119,95 @@ public class ToDoList {
 			+ todolist_ArrayList.get(i).getDescription());
 		}
 		System.out.println();
+		return todolist_ArrayList.get(i).getName();
 	}
+	public void ToDoListUpdate(ToDoListVO todolist_information,List<ToDoListVO> todolist_ArrayList){
+		String changeItemName="";
+		int changeItemIndex=0;
+		String newName="", newDescription="";
+		long newDate=0, newDue=0;
+		Collections.sort(todolist_ArrayList, new Comparator<ToDoListVO>() {
+			public int compare(ToDoListVO o1, ToDoListVO o2) {
+				if(o1.getDate()<o2.getDate()) {
+					return 1;
+				}
+				else if(o1.getDate()>o2.getDate()) {
+					return -1;
+				}
+				else return 0;
+				
+			}
+		});	
+		Scanner scan2 = new Scanner(System.in);
 	
+		System.out.println("name");
+		System.out.println("===================================================");
+		int j=todolist_ArrayList.size();
+		for(int i=0; i<todolist_ArrayList.size();i++) {
+			//System.out.println(todolist_information.getName()+todolist_information.getDate()+todolist_information.getDue()+todolist_information.getDescription());
+			System.out.println(todolist_ArrayList.get(i).getName() + "\t" );
+		}
+		
+		System.out.println();
+		System.out.print("Choice name");
+		changeItemName = scan2.nextLine();
+		for(int i=0; i<todolist_ArrayList.size();i++) {
+			
+			if((todolist_ArrayList.get(i).getName()).equals(changeItemName)) {
+				changeItemIndex=i;
+				//changeItemIndex=todolist_ArrayList.indexOf(changeItemName);
+				System.out.print("name: ");
+				newName=scan2.next();
+				//todolist_information.set(changeItemIndex,scan2.next());
+				System.out.print("date(YYMMDD): ");
+				newDate=scan2.nextLong();
+				//todolist_information.setDate(scan2.nextLong());
+				System.out.print("due(YYMMDD): ");
+				//todolist_information.setDue(scan2.nextLong());		
+				newDue=scan2.nextLong();
+				System.out.print("description: ");
+				//todolist_information.setDescription(scan2.next());
+				newDescription=scan2.next();
+				//System.out.print(changeItemIndex);
+				todolist_information= new ToDoListVO(newName, newDate, newDue, newDescription);
+				int result=this.update(changeItemIndex, todolist_information);
+			}
+		}	
+	}
+	public void ToDoListDelete(ToDoListVO todolist_information,List<ToDoListVO> todolist_ArrayList){
+		String changeItemName="";
+		int changeItemIndex=0;
+		String newName="", newDescription="";
+		long newDate=0, newDue=0;
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("name");
+		System.out.println("===================================================");
+		Collections.sort(todolist_ArrayList, new Comparator<ToDoListVO>() {
+			public int compare(ToDoListVO o1, ToDoListVO o2) {
+				if(o1.getDate()<o2.getDate()) {
+					return 1;
+				}
+				else if(o1.getDate()>o2.getDate()) {
+					return -1;
+				}
+				else return 0;
+				
+			}
+		});		
+		for(int i=0; i<todolist_ArrayList.size();i++) {
+			//System.out.println(todolist_information.getName()+todolist_information.getDate()+todolist_information.getDue()+todolist_information.getDescription());
+			System.out.println(todolist_ArrayList.get(i).getName() + "\t" );
+		}
+		
+		System.out.println();
+		System.out.print("Choice name");
+		changeItemName = scan2.nextLine();
+		for(int i=0; i<todolist_ArrayList.size();i++) {
+			if((todolist_ArrayList.get(i).getName()).equals(changeItemName)) {
+				todolist_ArrayList.remove(i);
+			}
+		}	
+	}	
 	public static void main(String[] args) {
 		ToDoList todolist_Program = new ToDoList();
 		todolist_Program.startProgram();
